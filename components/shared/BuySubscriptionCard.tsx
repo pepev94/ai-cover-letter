78d315f62e5e831808e3352c2dc3da68a38a565b
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-04-13 18:20:02 -0500

    removed uncesary code

--- components/shared/BuySubscriptionCard.tsx
+++ components/shared/BuySubscriptionCard.tsx
@@ -29,125 +29,128 @@ export const redirectToStripe = async () => {
   });
 };
 
-const proFeatures = [
-  <FormattedMessage
-    key="proFeature1"
-    id="proFeature1"
-    defaultMessage="Recipies AI"
-  />,
-  <FormattedMessage
-    key="proFeature1"
-    id="proFeature2"
-    defaultMessage="Recipies AI"
-  />,
-  <FormattedMessage
-    key="proFeature1"
-    id="proFeature3"
-    defaultMessage="Recipies AI"
-  />,
-  <FormattedMessage
-    key="proFeature1"
-    id="proFeature5"
-    defaultMessage="Recipies AI"
-  />,
-  <FormattedMessage
-    key="proFeature1"
-    id="proFeature4"
-    defaultMessage="Recipies AI"
-  />,
-];
+// const proFeatures = [
+//   <FormattedMessage
+//     key="proFeature1"
+//     id="proFeature1"
+//     defaultMessage="Recipies AI"
+//   />,
+//   <FormattedMessage
+//     key="proFeature1"
+//     id="proFeature2"
+//     defaultMessage="Recipies AI"
+//   />,
+//   <FormattedMessage
+//     key="proFeature1"
+//     id="proFeature3"
+//     defaultMessage="Recipies AI"
+//   />,
+//   <FormattedMessage
+//     key="proFeature1"
+//     id="proFeature5"
+//     defaultMessage="Recipies AI"
+//   />,
+//   <FormattedMessage
+//     key="proFeature1"
+//     id="proFeature4"
+//     defaultMessage="Recipies AI"
+//   />,
+// ];
 
 const FeaturesList = ({ list }: { list: JSX.Element[] }) => {
-  return (
-    <Box sx={{ p: 2, mt: 4 }}>
-      <Box>
-        {list.map((feature, i) => (
-          <Box key={i} sx={{ display: "flex" }}>
-            <CheckIcon sx={{ mr: 2, color: "#80ed9d" }} />
-            <Typography variant="h6" sx={{ color: "white" }} textAlign="left">
-              {feature}
-            </Typography>
-          </Box>
-        ))}
-      </Box>
-    </Box>
-  );
+  return null;
+
+  // return (
+  //   <Box sx={{ p: 2, mt: 4 }}>
+  //     <Box>
+  //       {list.map((feature, i) => (
+  //         <Box key={i} sx={{ display: "flex" }}>
+  //           <CheckIcon sx={{ mr: 2, color: "#80ed9d" }} />
+  //           <Typography variant="h6" sx={{ color: "white" }} textAlign="left">
+  //             {feature}
+  //           </Typography>
+  //         </Box>
+  //       ))}
+  //     </Box>
+  //   </Box>
+  // );
 };
 
 const BuySubscriptionCard = () => {
-  const session = useSession();
-  const [alignment, setAlignment] = useState<string | null>("pro");
-  const dispatch = useDispatch();
+  return null;
+  // const session = useSession();
+  // const [alignment, setAlignment] = useState<string | null>("pro");
+  // const dispatch = useDispatch();
 
-  const handleAlignment = (
-    event: React.MouseEvent<HTMLElement>,
-    newAlignment: string | null
-  ) => {
-    setAlignment(newAlignment);
-  };
+  // const handleAlignment = (
+  //   event: React.MouseEvent<HTMLElement>,
+  //   newAlignment: string | null
+  // ) => {
+  //   setAlignment(newAlignment);
+  // };
 
-  if (session.status === "loading") return null;
+  // if (session.status === "loading") return null;
 
-  if (session.status === "unauthenticated") return <LoginCta />;
+  // if (session.status === "unauthenticated") return <LoginCta />;
 
-  const handleCloseModal = () => {
-    dispatch(hideBuyMore());
-  };
+  // const handleCloseModal = () => {
+  //   dispatch(hideBuyMore());
+  // };
 
-  return (
-    <InfoModalCard>
-      <Typography
-        color="white"
-        sx={{
-          mt: 2,
-          whiteSpace: "pre-line",
-          textAlign: "left",
-          fontWeight: 700,
-        }}
-        variant="h5"
-        component="h1"
-      >
-        <FormattedMessage id="ranOutOfCredits" defaultMessage="Recipies AI" />
-      </Typography>
-      <ToggleButtonGroup
-        sx={{ mt: 4, backgroundColor: "white" }}
-        value={alignment}
-        color="standard"
-        exclusive
-        onChange={handleAlignment}
-        aria-label="text alignment"
-      >
-        <ToggleButton value="free" aria-label="centered">
-          Free
-        </ToggleButton>
-        <ToggleButton value="pro" aria-label="centered">
-          Pro
-        </ToggleButton>
-      </ToggleButtonGroup>
+  // return (
+  //   <InfoModalCard>
+  //     <Typography
+  //       color="white"
+  //       sx={{
+  //         mt: 2,
+  //         whiteSpace: "pre-line",
+  //         textAlign: "left",
+  //         fontWeight: 700,
+  //       }}
+  //       variant="h5"
+  //       component="h1"
+  //     >
+  //       <FormattedMessage id="ranOutOfCredits" defaultMessage="Recipies AI" />
+  //     </Typography>
+  //     <ToggleButtonGroup
+  //       sx={{ mt: 4, backgroundColor: "white" }}
+  //       value={alignment}
+  //       color="standard"
+  //       exclusive
+  //       onChange={handleAlignment}
+  //       aria-label="text alignment"
+  //     >
+  //       <ToggleButton value="free" aria-label="centered">
+  //         Free
+  //       </ToggleButton>
+  //       <ToggleButton value="pro" aria-label="centered">
+  //         Pro
+  //       </ToggleButton>
+  //     </ToggleButtonGroup>
 
-      <FeaturesList list={alignment === "pro" ? proFeatures : []} />
+  //     <FeaturesList list={alignment === "pro" ? proFeatures : []} />
 
-      {alignment === "pro" ? (
-        <Button
-          fullWidth
-          sx={{ mt: 6, color: "black", backgroundColor: "white" }}
-          variant="contained"
-          onClick={() => redirectToStripe()}
-        >
-          <FormattedMessage id="buyTokensCTA" />
-        </Button>
-      ) : (
-        <Button
-          fullWidth
-          sx={{ mt: 6, color: "black", backgroundColor: "white" }}
-          variant="contained"
-          onClick={() => handleCloseModal()}
-        >
-          <FormattedMessage id="stayInFree" />
-        </Button>
-      )}
-    </InfoModalCard>
-  );
+  //     {alignment === "pro" ? (
+  //       <Button
+  //         fullWidth
+  //         sx={{ mt: 6, color: "black", backgroundColor: "white" }}
+  //         variant="contained"
+  //         onClick={() => redirectToStripe()}
+  //       >
+  //         <FormattedMessage id="buyTokensCTA" />
+  //       </Button>
+  //     ) : (
+  //       <Button
+  //         fullWidth
+  //         sx={{ mt: 6, color: "black", backgroundColor: "white" }}
+  //         variant="contained"
+  //         onClick={() => handleCloseModal()}
+  //       >
+  //         <FormattedMessage id="stayInFree" />
+  //       </Button>
+  //     )}
+  //   </InfoModalCard>
+  // );
 };
 
 export default BuySubscriptionCard;
