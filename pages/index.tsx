7aca60425a7308581b9d9ef90dea149a3457f73e
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-04-14 15:31:50 -0500

    added tag to head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import CreateCV from "@/components/CreateCV";
 import Seo from "@/components/SEO/Seo";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="5IRS4lod80OkHaQEfkyPAejW_GLUr_cY1jKlx8g2in0"
+        />
+      </Head>
       <Seo />
       <CreateCV />
     </>
